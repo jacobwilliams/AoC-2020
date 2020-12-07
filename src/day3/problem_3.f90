cbd47825b1b5e22c996f396266df584c38ca26df
Jacob Williams <jacobwilliams@users.noreply.github.com>
2020-12-06 20:37:47 -0600

    3b

--- src/day3/problem_3.f90
+++ src/day3/problem_3.f90
@@ -3,12 +3,16 @@ program problem_3
 !! how many trees would you encounter?
 
 use aoc_utilities
+use iso_fortran_env
 
 implicit none
 
-integer :: iunit, istat, n_lines, i, j, n_cols, n_trees, col
+integer:: iunit, istat, n_lines, i, j, n_cols, col
 character(len=1000) :: line
 logical,dimension(:,:),allocatable :: data
+integer,dimension(5),parameter :: right = [1,3,5,7,1]
+integer,dimension(5),parameter :: down  = [1,1,1,1,2]
+integer(int64) :: n_trees, product ! to avoid overflow
 
 ! read into a logical array (true=true)
 open(newunit=iunit, file='input.txt', iostat=istat)
@@ -26,6 +30,11 @@ do i = 1, n_lines
     end do
 end do
 
+!**********************
+
+write(*,*) ''
+write(*,*) 'part a:'
+
 col = 0
 n_trees = 0
 do i = 1, n_lines
@@ -34,5 +43,22 @@ do i = 1, n_lines
 end do
 
 write(*,*) 'n_trees = ', n_trees
+
+!**********************
+
+write(*,*) ''
+write(*,*) 'part b:'
+
+product = 1
+do j = 1, 5
+    col = 0
+    n_trees = 0
+    do i = 1, n_lines, down(j)
+        if (data(i,col+1)) n_trees = n_trees + 1
+        col = mod(col + right(j), n_cols)
+    end do
+    product = product * n_trees
+end do
+write(*,*) 'product = ', product
 
 end program problem_3
