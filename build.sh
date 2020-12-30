4cd6426d69ed38f290c8a703c99332cc21d270b3
Jacob Williams <jacobwilliams@users.noreply.github.com>
2020-12-30 10:02:59 -0600

    added basic build script and workspace file

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/zsh
+
+#source /opt/intel/oneapi/setvars.sh
+
+# rm -rf ./bin
+# mkdir ./bin 
+
+COMPILER=ifort
+
+# rm -rf ./src/day1/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day1/problem_1a.f90 -o ./src/day1/_prob1a
+# $COMPILER ./src/aoc_utilities.f90 ./src/day1/problem_1b.f90 -o ./src/day2/_prob1b
+
+# rm -rf ./src/day2/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day2/problem_2.f90 -o  ./src/day2/_prob2
+# $COMPILER ./src/aoc_utilities.f90 ./src/day2/problem_2.f90 -o  ./src/day2/_prob2
+
+# rm -rf ./src/day3/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day3/problem_3.f90 -o  ./src/day3/_prob3
+
+# rm -rf ./src/day4/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day4/problem_4.f90 -o   ./src/day4/_prob4
+# $COMPILER ./src/aoc_utilities.f90 ./src/day4/problem_4b.f90 -o  ./src/day4/_prob4b
+
+# rm -rf ./src/day5/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day5/problem_5.f90 -o  ./src/day5/_prob5
+
+# rm -rf ./src/day6/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day6/problem_6.f90 -o  ./src/day6/_prob6
+
+# rm -rf ./src/day7/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day7/problem_7.f90 -o  ./src/day7/_prob7
+
+# rm -rf ./src/day8/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day8/problem_8.f90 -o  ./src/day8/_prob8
+
+# rm -rf ./src/day9/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day9/problem_9.f90 -o  ./src/day9/_prob9
+
+# rm -rf ./src/day10/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day10/problem_10.f90 -o  ./src/day10/_prob10
+
+# rm -rf ./src/day11/_*
+# $COMPILER ./src/aoc_utilities.f90 ./src/day11/problem_11.f90 -o  ./src/day11/_prob11
+
+rm -rf ./src/day12/_*
+$COMPILER ./src/aoc_utilities.f90 ./src/day12/problem_12.f90 -o  ./src/day12/_prob12
+
+
+
+
+
+
+
